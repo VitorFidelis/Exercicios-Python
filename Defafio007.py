928f4d9e060033f90538f876c14643de464035a9
Vitor Fidelis <109974299+VitorFidelis@users.noreply.github.com>
2022-10-25 22:34:55 -0300

    Delete Defafio007.py

--- Defafio007.py
+++
@@ -1,6 +0,0 @@
-print('=====','DESAFIO 007','=====')
-
-n1=int(input('Digite a primeira nota:'))
-n2=int(input('Digite a segunda nota:'))
-print('O valor da soma entre as duas notas é:{}\n'
-      'e a média das duas notas é:{}'.format(n1+n2,n1+n2/6))
